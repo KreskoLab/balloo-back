ecc6bc9360d7fd53f77ee2722e816d1bf22cde28
KreskoLab <redeezol@gmail.com>
2022-05-03 15:13:29 +0300

    feat (utils): add function to check if folder exist

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,12 @@
 import fs from 'fs/promises';
+import { constants } from 'fs';
 import { v4 as uuidv4 } from 'uuid';
 import slugify from 'slugify';
 import path from 'path';
 
 export async function saveImage(file: any): Promise<string> {
+  await folderExist();
+
   const buffer = await file.toBuffer();
   const ext = path.extname(file.filename);
   const name = uuidv4();
@@ -12,6 +15,14 @@ export async function saveImage(file: any): Promise<string> {
   await fs.writeFile(folder + name + ext, buffer);
 
   return name + ext;
+}
+
+async function folderExist(): Promise<void> {
+  try {
+    await fs.access(`${process.cwd()}/images`, constants.R_OK | constants.W_OK);
+  } catch (error) {
+    await fs.mkdir(`${process.cwd()}/images`);
+  }
 }
 
 export async function removeImage(image: string): Promise<void> {
